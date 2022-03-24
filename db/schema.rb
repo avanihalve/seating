85b12f52cc25548e191f40ce219f4402eddac6b7
Avani Halve <avani.halve@gmail.com>
2022-03-24 15:54:45 +0530

    change database

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_21_045546) do
+ActiveRecord::Schema.define(version: 2022_03_24_101818) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
-    t.integer "record_id", null: false
-    t.integer "blob_id", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
     t.datetime "created_at", null: false
     t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
     t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
@@ -40,7 +43,7 @@ ActiveRecord::Schema.define(version: 2022_03_21_045546) do
     t.string "contact"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "seat_id"
+    t.bigint "seat_id"
     t.string "email"
     t.index ["seat_id"], name: "index_employees_on_seat_id"
   end
